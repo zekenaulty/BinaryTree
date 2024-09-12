3c6c61a9bcc4415670ab6f4da56cf7899a8aedf0
Zeke <zekenaulty>
2024-09-12 16:00:26 -0500

    ok, I hate bundlers and think they are pointless, because they are, but whatever (kidding, I just hate fixing stuff that should just work)

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,17 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  root: '.', // Set the root directory to the current folder if it's not src
+  root: '.', // Root directory is the project root
   build: {
     rollupOptions: {
-      input: './main.js', // Specify your custom entry point
+      input: {
+        main: './index.html', // Specify the correct entry point
+      },
     },
     outDir: 'dist', // Output directory
+    emptyOutDir: true, // Ensure the dist directory is cleaned before each build
   },
   server: {
-    port: 3000, // You can specify a port for the dev server
+    port: 3000, // Dev server port
   }
 });
